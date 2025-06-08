a292fb3e78a8ae7905a8c9f04dde87c4f6343e9b
leobenTaningco <leotaningco@gmail.com>
2025-06-08 18:56:35 +0800

    fix: reply success notification

--- app/Http/Controllers/ReplyController.php
+++ app/Http/Controllers/ReplyController.php
@@ -25,7 +25,7 @@ class ReplyController extends Controller
         ]);
 
         Reply::create($validated);
-        return redirect()->back();
+        return redirect()->back()->with('success', 'Reply added!');
     }
     
     public function destroy(Reply $reply){
